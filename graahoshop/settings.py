43ecfdcf9fef2ac7266b30c693ac5d8e80d981c6
Mobasshir Bhuiyan Shagor <mobasshirbhuiyan.shagor@gmail.com>
2019-06-10 14:38:48 +0600

    after heroku failed push

--- graahoshop/settings.py
+++ graahoshop/settings.py
@@ -202,10 +202,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 # STATIC_ROOT = location('static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
