d65f81a302011b17ff143a51d718aa555be58f86
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2019-06-23 12:26:51 +0600

    Added weight based shipping method

--- apps/shipping/repository.py
+++ apps/shipping/repository.py
@@ -16,24 +16,34 @@
 # class Repository(CoreRepository):
 #     methods = (method1, method2,)
 
+from decimal import Decimal as D
+from oscar.apps.shipping.methods import Free, FixedPrice, NoShippingRequired
+from oscar.apps.shipping.repository import Repository as CoreRepository
+from oscar.apps.shipping import methods, models
+from oscar.apps.shipping.models import WeightBased
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
 
-from . import methods
-from oscar.core.loading import get_model
-from oscar.apps.shipping import repository
+# hand delivery, eg for customers located near company premises
+class HandDelivery(methods.NoShippingRequired):
+    code = "hand-delivery"
+    name = _("Hand Delivery")
+    description = _("No shipping required. Product will be delivered hand to hand.")
+    charge_excl_tax = D('00.00')
 
-WeightBased = get_model('shipping', 'WeightBased')
+class Repository(CoreRepository):
+    methods = [HandDelivery()]
 
-
-class Repository(repository.Repository):
-    def get_available_shipping_methods(self, basket, user=None,
-                                       shipping_addr=None, request=None, **kwargs):
-
+    def get_available_shipping_methods(self, basket, user=None, shipping_addr=None,request=None, **kwargs):
         if shipping_addr:
-            weightbased_set = WeightBased.objects.all()
-
+            weightbased_set = WeightBased.objects.all().filter(
+                countries=shipping_addr.country.code)
             if weightbased_set:
-                return (list(weightbased_set), )
+                methods = (list(weightbased_set))
+                methods += [HandDelivery()]
+            else:
 
-        # If no address was specified, or weight-based options are
-        # not available, return the "Reserve" shipping option
-        return (methods.Reserve(), )
+                methods = []
+        else:
+            methods = [HandDelivery()]
+        return methods
